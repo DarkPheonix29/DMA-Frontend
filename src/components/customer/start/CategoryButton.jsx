33dbc7ada1289752873f658f5df1943b48a59598
Kian Bruyenberg <kbruyenberg@gmail.com>
2025-03-27 15:34:13 +0100

    Add RequestWaiter component and enhance CategoryButton with images
    - Introduced RequestWaiter component for requesting a waiter.
    - Updated CategoryButton to accept and display an image alongside the text.
    - Modified CategoryList to include images for each category.
    - Adjusted styles for buttons and layout in MenuStartPage.
    - Added custom pastel color theme in index.css.

--- src/components/customer/start/CategoryButton.jsx
+++ src/components/customer/start/CategoryButton.jsx
@@ -1,9 +1,10 @@
-const CategoryButton = ({ text, onClick }) => {
+const CategoryButton = ({ text, image, onClick }) => {
   return (
     <button
         onClick={onClick}
-        className="px-6 py-3 bg-blue-600 text-white rounded-2xl font-semibold shadow-md transition hover:bg-blue-700"
+        className="px-6 py-3 bg-pastelred-400 text-black rounded-lg font-semibold shadow-md basis-35 border border-black overflow-hidden"
     >
+      <img src={image} alt={text} className="aspect-square object-cover" />
         {text}
     </button>
   );
--- src/pages/customer/MenuStartPage.jsx
+++ src/pages/customer/MenuStartPage.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import CategoryButton from '../../components/customer/start/CategoryButton';
 import CategoryList from '../../components/customer/start/CategoryList';
+import RequestWaiter from '../../components/customer/RequestWaiter';
 
 const MenuStartPage = () => {
     return (
         <div>
-        <h1>Customer Menu Start Page</h1>
-        <CategoryList />
+            <div><p className='absolute underline right-4 top-3 font-semibold text-white'>Table 11</p></div> {/* To be changed with qr function */}
+            <div className='min-h-screen flex flex-col justify-items-start gap-[10vh] bg-pastelred-700 p-6'>
+                <RequestWaiter />
+                <CategoryList />
+            </div>
         </div>
+        
     );
 }
 
