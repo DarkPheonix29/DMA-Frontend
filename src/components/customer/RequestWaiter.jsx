ab555dddfa784fa876ad593a2176545eab8ea4d7
Kian Bruyenberg <kbruyenberg@gmail.com>
2025-04-10 13:24:07 +0200

    Refactor button components to accept className prop for better styling flexibility and update layout in MenuStartPage

--- src/components/customer/RequestWaiter.jsx
+++ src/components/customer/RequestWaiter.jsx
@@ -1,10 +1,10 @@
 import RequestWaiterImg from "../../assets/images/requestWaiter.png";
 
-const RequestWaiter = () => {
+const RequestWaiter = ({ className = ""}) => {
     return (
         <button 
             onClick={() => alert("Waiter has been requested")} 
-            className="px-3 py-3 bg-white text-black rounded-lg font-semibold shadow-md basis-10 border border-black size-fit"
+            className={`px-3 py-3 bg-white text-black rounded-lg font-semibold shadow-md basis-10 border border-black size-fit ${className}`}
         >
             <div className="flex flex-row items-center gap-2">
                 <img src={RequestWaiterImg} alt="Request the waiter" className="aspect-square object-scale-down w-7 h-7" />
