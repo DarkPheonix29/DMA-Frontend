19bff2cb41eb47db4ba6bede6642cfa02a1207b4
Fantasyfays <134273551+Fantasyfays@users.noreply.github.com>
2025-05-16 11:17:21 +0200

    bestellen werkend gekregen

--- src/components/customer/menu/MenuList.jsx
+++ src/components/customer/menu/MenuList.jsx
@@ -1,21 +1,21 @@
 import MenuItem from "./MenuItem";
 
 const MenuList = ({ items, onItemClick }) => {
-    console.log(items);
     return (
-    <ul className="flex flex-col items-center w-full mt-30">
-        {items.length > 0 ? (
-            items.map((item) => (
-                <MenuItem
-                    key={item.id}
-                    item={item}
-                    onClick={() => onItemClick(item)}
-                />
-            ))
-        ) : (
-            <p className="text-gray-500">Geen items gevonden.</p>
-        )}
-    </ul>
+        <ul className="flex flex-col items-center w-full mt-30">
+            {items.length > 0 ? (
+                items.map((item) => (
+                    <li key={item.id} className="w-full">
+                        <MenuItem
+                            item={item}
+                            onClick={() => onItemClick(item)}
+                        />
+                    </li>
+                ))
+            ) : (
+                <p className="text-gray-500">Geen items gevonden.</p>
+            )}
+        </ul>
     );
 };
 
