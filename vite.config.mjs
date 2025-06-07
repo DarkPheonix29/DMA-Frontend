da3b325839a69bb87963a91c509f121de6e12044
Sterre van der Klauw <samvanderklauw@gmail.com>
2025-06-07 13:39:35 +0200

    Changed from http to https and made the pem files for that

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,23 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import fs from 'fs';
+import path from 'path';
 
 export default defineConfig(() => {
   return {
     plugins: [
-        react(),
-        tailwindcss(),
+      react(),
+      tailwindcss(),
     ],
     server: {
-      proxy: {
-          '^/api/': {
-              target: 'https://localhost:7117', //32769
-
-              secure: false,
-              changeOrigin: true
-          }
+      https: {
+        key: fs.readFileSync(path.resolve(__dirname, 'cert/localhost-key.pem')),
+        cert: fs.readFileSync(path.resolve(__dirname, 'cert/localhost.pem')),
       },
-      port: 50623
-  }
+      proxy: {
+        '^/api/': {
+          target: 'https://localhost:7117',
+          secure: false,
+          changeOrigin: true,
+        }
+      },
+      port: 50623,
+    }
   };
 });
